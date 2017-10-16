38a151eb1645460e369de0eb42cc0c4c16f364c7
Richard Luo <cdominant7@gmail.com>
2017-10-16 18:29:59 +0800

    using blue-mood

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,6 +1,14 @@
+(require-package 'color-theme-modern)
+
+;; (load-theme 'deeper-blue t t)
+;; (enable-theme 'deeper-blue)
+
+;; load your favorite theme
 (load-theme 'blue-mood t t)
 (enable-theme 'blue-mood)
+
 (setq frame-resize-pixelwise t)
 (set-frame-position (selected-frame) 50 0)
 (set-frame-size (selected-frame) 1300 820 t)
+
 (provide 'init-themes)
