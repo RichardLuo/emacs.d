ad0005bbd944b366cc2e38f38cd94b8617c0682b
Richard Luo <cdominant7@gmail.com>
2021-06-15 14:40:01 +0800

    delete unused code in eshell

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -76,27 +76,27 @@ FILENAME defaults to `buffer-file-name'."
   (file-name-sans-extension
    (file-name-nondirectory (or filename (buffer-file-name)))))
 
-(defun shortened-path (path max-len)
-  "Return a modified version of `path', replacing some components
-      with single characters starting from the left to try and get
-      the path down to `max-len'"
-  (let* ((components (split-string (abbreviate-file-name path) "/"))
-         (len (+ (1- (length components))
-                 (reduce '+ components :key 'length)))
-         (str ""))
-    (while (and (> len max-len)
-                (cdr components))
-      (setq str (concat str (if (= 0 (length (car components)))
-                                "/"
-                              (string (elt (car components) 0) ?/)))
-            len (- len (1- (length (car components))))
-            components (cdr components)))
-    (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
+;; (defun shortened-path (path max-len)
+;;   "Return a modified version of `path', replacing some components
+;;       with single characters starting from the left to try and get
+;;       the path down to `max-len'"
+;;   (let* ((components (split-string (abbreviate-file-name path) "/"))
+;;          (len (+ (1- (length components))
+;;                  (reduce '+ components :key 'length)))
+;;          (str ""))
+;;     (while (and (> len max-len)
+;;                 (cdr components))
+;;       (setq str (concat str (if (= 0 (length (car components)))
+;;                                 "/"
+;;                               (string (elt (car components) 0) ?/)))
+;;             len (- len (1- (length (car components))))
+;;             components (cdr components)))
+;;     (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
 
-(setq eshell-prompt-function
-      (lambda ()
-      (concat (shortened-path (eshell/pwd) 40)
-              (if (= (user-uid) 0) " # " " $ "))))
+;; (setq eshell-prompt-function
+;;       (lambda ()
+;;       (concat (shortened-path (eshell/pwd) 40)
+;;               (if (= (user-uid) 0) " # " " $ "))))
 
 ;; (setq eshell-prompt-function
 ;;       (lambda ()
