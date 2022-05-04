aa0efa994318dc7ee763e65fb46cc172eb8d1805
richard <richard@Richard-Luo.local>
2022-05-04 21:35:08 +0800

    using icomplete for emacs with version 28.1 or greater instead of ido

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,22 +1,28 @@
-;; enable ido-mode, flexible-matching, virtual-buffer
-(require 'ido)
-
-(defvar ido-cur-item nil)
-(defvar ido-default-item nil)
-(defvar ido-cur-list nil)
-
-(ido-mode t)
-(ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(setq ido-use-virtual-buffers t)
-
-;; use ido-ubiquitous
-(require-package 'ido-ubiquitous)
-(ido-ubiquitous-mode t)
-
-;; use smex to handle M-x
-(require-package 'smex)
-(smex-initialize)
-(global-set-key "\M-x" 'smex)
+(if (version< emacs-version "28.1")
+    (progn
+      (progn
+        ;; make buffer switch command do suggestions, also for find-file command
+        (require 'ido)
+        (ido-mode 1)
+        ;; show choices vertically
+        (setf (nth 2 ido-decorations) "\n")
+        ;; show any name that has the chars you typed
+        (setq ido-enable-flex-matching t)
+        ;; use current pane for newly opened file
+        (setq ido-default-file-method 'selected-window)
+        ;; use current pane for newly switched buffer
+        (setq ido-default-buffer-method 'selected-window)
+        )
+      (progn
+        ;; minibuffer enhanced completion icomplete
+        (require 'icomplete)
+        (icomplete-mode 1)
+        ;; show choices vertically
+        (setq icomplete-separator "\n")
+        (setq icomplete-hide-common-prefix nil)
+        (setq icomplete-in-buffer t)
+        (define-key icomplete-minibuffer-map (kbd "<right>") 'icomplete-forward-completions)
+        (define-key icomplete-minibuffer-map (kbd "<left>") 'icomplete-backward-completions)))
+  (fido-vertical-mode 1))
 
 (provide 'init-ido)
