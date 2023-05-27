ea75a27e6e41b4edbb4dddc6107f86c4c4147875
Richard Luo <cdominant7@gmail.com>
2023-05-27 23:00:32 +0800

    using new theme from emacs-from-scratch, using the doom

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,14 +1,12 @@
-(require-package 'color-theme-modern)
+;; (require-package 'color-theme-modern)
+;; (load-theme 'blue-mood t t)
+;; (enable-theme 'blue-mood)
+;; (setq frame-resize-pixelwise t)
+;; (set-frame-position (selected-frame) 50 0)
+;; (set-frame-size (selected-frame) 1300 820 t)
 
-;; (load-theme 'deeper-blue t t)
-;; (enable-theme 'deeper-blue)
+(use-package doom-themes
+  :init (load-theme 'doom-palenight t))
 
-;; load your favorite theme
-(load-theme 'blue-mood t t)
-(enable-theme 'blue-mood)
-
-(setq frame-resize-pixelwise t)
-(set-frame-position (selected-frame) 50 0)
-(set-frame-size (selected-frame) 1300 820 t)
 
 (provide 'init-themes)
