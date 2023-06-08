c82e47203d34ed405ec0b98f7a0df3bbd104b261
richard <richard@Richard-Luo.lan>
2023-06-08 15:34:37 +0800

    using system-type to differ osx and linux, set different font and themes

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -7,9 +7,9 @@
 ;;         (nil))) ;; if on term
 
 
-(set-frame-font "Monaco:size=15")
-(set-face-attribute 'default nil :font "Monaco-15")
-(setq default-frame-alist nil)
+;; (set-frame-font "Monaco:size=15")
+;; (set-face-attribute 'default nil :font "Monaco-15")
+;; (setq default-frame-alist nil)
 
 ;; (load-theme 'sanityinc-tomorrow-bright t)
 
--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,8 +5,20 @@
 ;; (set-frame-position (selected-frame) 50 0)
 ;; (set-frame-size (selected-frame) 1300 820 t)
 
-(use-package doom-themes
-  :init (load-theme 'doom-palenight t))
+(when (eq system-type 'darwin) ;; mac specific settings
+  (set-frame-font "Monaco:size=15")
+  (set-face-attribute 'default nil :font "Monaco-15")
+  (setq default-frame-alist nil)
+  (use-package nord-theme
+    :init (load-theme 'nord t))
+  )
 
+(when (eq system-type 'gnu/linux) ;; mac specific settings
+  (set-frame-font "Monaco:size=15")
+  (set-face-attribute 'default nil :font "Monaco-15")
+  (setq default-frame-alist nil)
+  (use-package doom-themes
+    :init (load-theme 'doom-palenight t))
+  )
 
 (provide 'init-themes)
