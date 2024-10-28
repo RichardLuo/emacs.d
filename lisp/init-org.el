5674e5042834d42cadf712aa9c43fe429549be86
richard <richard@Richard-Luo.local>
2024-10-28 13:10:40 +0800

    beautified or-mode with file init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,3 @@
-;; 确保你有一个Emacs版本至少为26.1
-;; 将这段代码添加到你的init.el文件中，或者如果你使用的是Doom Emacs或Spacemacs，按照相应的方式添加到配置中
-
 ;; Org-mode基础配置
 (use-package org
   :ensure t
@@ -12,11 +9,12 @@
 
   (setq org-log-done 'note)  ;; 当任务完成时，提示输入注释并记录时间
 
+
   ;; 设置Org目录和默认Org文件
   (setq org-directory (list "~/org" "~/obsidian-techspec/org"))
   (setq org-default-notes-file (expand-file-name "notes.org" (car org-directory)))
 
-  ;; 启用Org-indent模式，美化文档结构
+  ;; Org-indent模式，用于美化文档结构
   (setq org-startup-indented t)
 
   ;; 配置TODO关键词的外观
@@ -61,36 +59,28 @@
   ;; 确保在执行代码块前进行确认
   (setq org-confirm-babel-evaluate nil)
 
-  (setq org-capture-templates
-        '(("n" "New Org Entry" plain
-           (file (lambda () (buffer-file-name)))
-           "#+TITLE: %^{Title}\n#+AUTHOR: York.Lee\n#+EMAIL: york.lee@gmail.com\n#+DATE: %U\n#+DESCRIPTION: %^{Description}\n\n"
-           :empty-lines 0))) ;; 确保没有多余的空行
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/tasks.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("n" "Note" entry (file "~/org/notes.org")
+         "* %U %?\n  %i\n  %a")
+        ("N" "New Org Entry" plain
+         (file (lambda () (buffer-file-name)))
+         "#+TITLE: %^{Title}\n#+AUTHOR: York.Lee\n#+EMAIL: york.lee@gmail.com\n#+DATE: %U\n#+DESCRIPTION: %^{Description}\n\n"
+         :empty-lines 0)
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+         "* %U %?\n  %i\n  %a")
+        ("b" "Bookmark" entry (file "~/org/bookmarks.org")
+         "* %?  \n %u\n  %a\n[[bookmark:%s][My Bookmark]]")))
   )
 
-;; Org-bullets，用漂亮的符号替换标准的Org模式的列表符号
-(use-package org-bullets
-  :ensure t
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-;; 启用Org-mode和相关包
-(require 'org)
-(require 'org-bullets)
-
-;; 提醒：确保你已经安装了use-package和org-bullets
-;; 你可能需要在配置中添加以下行来安装use-package:
-;; (unless (package-installed-p 'use-package)
-;;   (package-refresh-contents)
-;;   (package-install 'use-package))
-
-;; (setq-default line-spacing 0.2)  ;; 调整行间距
-;; (setq-default default-text-properties '(line-height 1.0))  ;; 调整字符间距
-
-;; (require 'ox-md)
-
+;; ;; Org-bullets，用漂亮的符号替换标准的Org模式的列表符号
+;; (use-package org-bullets
+;;   :ensure t
+;;   :after org
+;;   :hook (org-mode . org-bullets-mode)
+;;   :custom
+;;   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (use-package ox-pandoc
   :ensure t  ;; 确保ox-pandoc插件已安装
@@ -107,16 +97,6 @@
   '(add-to-list 'org-export-backends 'pandoc))
 
 
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/tasks.org" "Tasks")
-         "* TODO %?\n  %i\n  %a")
-        ("n" "Note" entry (file "~/org/notes.org")
-         "* %U %?\n  %i\n  %a")
-        ("j" "Journal" entry (file+datetree "~/org/journal.org")
-         "* %U %?\n  %i\n  %a")
-        ("b" "Bookmark" entry (file "~/org/bookmarks.org")
-         "* %?  \n %u\n  %a\n[[bookmark:%s][My Bookmark]]")))
-
 (use-package org-cliplink
   :ensure t
   :commands (org-cliplink-capture)
@@ -124,14 +104,96 @@
   (setq org-cliplink-default-description-style 'title)  ; 使用页面标题作为描述
 )
 
-(defun org-gen-link-file-line-no ()
-  "Insert a link to the current file and line number in org-mode format into the clipboard."
+(defun get-right-of-cursor ()
   (interactive)
-  (let* ((line-number (line-number-at-pos))  ; 获取当前行号
-         (file-name (file-name-nondirectory (buffer-file-name)))  ; 获取文件名
-         (description (read-string "Enter description: "))  ; 提示输入描述
+  (let* ((start-pos (point))
+         (end-pos (min (line-end-position) (+ start-pos 60)))  ; 计算最多60个字符的位置，不超过行末
+         (right-string (buffer-substring-no-properties start-pos end-pos)))  ; 获取右侧字符串
+    (message "Right string: %s" right-string)))
+
+(defun org-gen-file-link-line-no ()
+  "Insert a link to the current file and line number in org-mode format into the clipboard, with a generated description."
+  (interactive)
+  (let* ((line-number (line-number-at-pos))
+         (file-name (file-name-nondirectory (buffer-file-name)))
+         (start-pos (point))
+         (end-pos (min (line-end-position) (+ start-pos 60)))  ; 计算最多60个字符的位置，不超过行末
+         (description (buffer-substring-no-properties start-pos end-pos))  ; 获取右侧字符串作为描述
          (link (format "[[file:%s::%d][%s]]" file-name line-number description)))  ; 生成链接
-    (kill-new link)  ; 将链接放入粘贴板
-    (message "Link copied to clipboard: %s" link)))  ; 提示用户链接已复制
+    (kill-new link)
+    (message "Link copied to clipboard: %s" link)
+    (let ((user-description (read-string "Edit description: " description)))
+      (kill-new (format "[[file:%s::%d][%s]]" file-name line-number user-description)))))
+
+
+(defun org-gen-abs-file-link-line-no ()
+  "Insert a link to the current file (absolute path) and line number in org-mode format into the clipboard, with a generated description."
+  (interactive)
+  (let* ((line-number (line-number-at-pos))
+         (file-name (expand-file-name (buffer-file-name)))  ; 获取绝对路径
+         (start-pos (point))
+         (end-pos (min (line-end-position) (+ start-pos 60)))
+         (description (buffer-substring-no-properties start-pos end-pos))
+         (link (format "[[file:%s::%d][%s]]" file-name line-number description)))
+    (kill-new link)
+    (message "Link copied to clipboard: %s" link)
+    (let ((user-description (read-string "Edit description: " description)))
+      (kill-new (format "[[file:%s::%d][%s]]" file-name line-number user-description)))))
+
+;; (use-package org-appear
+;;   :ensure t
+;;   :hook (org-mode . org-appear-mode)  ;; 在 org-mode 启动时自动启用 org-appear
+;;   :config
+;;   (setq org-appear-autolinks t           ;; 自动显示链接
+;;         org-appear-autosubmarkers t      ;; 自动显示子标记（如 /斜体/ 等）
+;;         org-appear-autokeywords t        ;; 自动显示关键词标记（如 TODO、DONE 等）
+;;         org-appear-delay 0.5             ;; 设置延迟显示（秒）
+;;         org-appear-trigger 'manual))     ;; 设置触发条件（manual为手动触发）
+
+;; (use-package org-appear
+;;   :ensure t
+;;   :hook (org-mode . org-appear-mode))
+
+(use-package org-appear
+  :ensure t
+  :after org
+  :hook (org-mode . org-appear-mode)
+  :config
+  (setq org-hide-emphasis-markers t       ;; 确保隐藏加粗、斜体等符号
+        org-appear-autoemphasis t         ;; 启用强调符号的自动显示
+        org-appear-autolinks t            ;; 启用链接的自动显示
+        org-appear-autosubmarkers t       ;; 启用上标和下标符号的自动显示
+        org-appear-autoentities t         ;; 启用 Org entities 的自动显示
+        org-appear-autokeywords t         ;; 启用关键字的自动显示
+        org-appear-inside-latex t         ;; 启用 LaTeX 片段符号的自动显示
+        org-appear-delay 0.5              ;; 设置延迟时间为 0.5 秒
+        org-appear-trigger 'manual))      ;; 设置手动触发
+
+
+(use-package org-modern
+  :ensure t
+  :hook (org-mode . org-modern-mode)
+  :config
+  ;; 如果使用 org-superstar，可以关闭 org-modern 的标题符号设置
+  (setq org-modern-star nil)
+  ;; 保留其他 org-modern 的美化功能
+  (setq org-modern-list nil)  ;; 如果想让列表符号也用 org-superstar，禁用 org-modern 的列表符号
+  )
+
+(use-package org-superstar
+  :ensure t
+  :hook (org-mode . org-superstar-mode)
+  :config
+  (setq org-superstar-headline-bullets-list '("◉" "○" "✸" "◆" "▶")
+
+        org-superstar-item-bullet-alist '((?+ . ?●)   ;; 大黑点
+                                          (?* . ?■)   ;; 实心方块
+                                          (?- . ?◆))
+
+        ;; org-superstar-item-bullet-alist '((?+ . ?•)   ;; 小圆点
+        ;;                                   (?* . ?▪)   ;; 实心小方块
+        ;;                                   (?- . ?‣))
+
+        )
 
 (provide 'init-org)
